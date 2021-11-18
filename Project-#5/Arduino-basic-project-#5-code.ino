9efa0f1eb9b96afc12adeed05338ea0de7c15383
Monmoy042 <monmoykms.16@gmail.com>
2021-11-18 19:18:24 +0600

    Start working on Project-#5

---
+++ Project-#5/Arduino-basic-project-#5-code.ino
@@ -0,0 +1,23 @@
+// Date: 18/11/21
+// Khaled Md. Saifullah
+// KastTech
+
+int potPin= A0;
+int LEDPin= 9;
+int potValue;
+int colorLevel;
+
+void setup() {
+  pinMode(potPin, INPUT);
+  pinMode(LEDPin, OUTPUT);
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  
+ potValue = analogRead(potPin);
+ colorLevel = map(potValue,1,1024,1,255);
+ analogWrite(LEDPin, colorLevel);
+ Serial.print("The value of Resistance is: ");
+ Serial.println(colorLevel);
+}
---
+++ Project-#5/README.md
@@ -0,0 +1,16 @@
+# Arduino Basic Project (LED brightness control using Potentiometer)
+
+## Github Repository Link
+```
+
+```
+
+## Tinkercad Project Link
+```
+
+```
+
+## Hackster Project Link
+```
+
+```
